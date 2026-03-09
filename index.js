3b850c23a49af38f18fb82ec9985346f2cccb663
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-03-10 01:51:41 +0600

    Database setUp

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const corsOptions={
+    origin:['http://localhost:5173'],
+    credentials:true,
+}
+app.use(cors(corsOptions))
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4imj4lo.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send("SoloSphere search jobs");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on :http://localhost:${port}`);
+    
+})
